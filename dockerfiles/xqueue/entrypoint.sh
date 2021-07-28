0edbd441b516a7485ce9cc1c2ebe798c89c74909
cassidy <cassidy@foghornconsulting.com>
2021-07-28 18:19:06 -0400

    add dependencies, pull config from ssm

--- dockerfiles/xqueue/entrypoint.sh
+++ dockerfiles/xqueue/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+set -eux
+
+export XQUEUE_CFG=/edx/app/xqueue/xqueue_cfg.yml
 export DJANGO_SETTINGS_MODULE=xqueue.production
+SSM_CONFIG=/mitxonline-qa/xqueue/xqueue_cfg
+
+aws ssm get-parameter --name "${SSM_CONFIG}" --with-decryption | jq -r '.Parameter.Value' > "${XQUEUE_CFG}"
 
 ./manage.py migrate
 ./manage.py update_users
 gunicorn -c /edx/app/xqueue/xqueue/docker_gunicorn_configuration.py --bind=0.0.0.0:8040 --workers 2 --max-requests=1000 xqueue.wsgi:application
-
